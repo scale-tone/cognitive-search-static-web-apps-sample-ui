f60769acbd51d9e0a22aaf77b4bb3465fa3e75ec
Konstantin Lepeshenkov <kolepes@microsoft.com>
2020-09-02 12:25:55 +0200

    minor fixes

--- api/HttpTest/index.ts
+++ api/HttpTest/index.ts
@@ -1,11 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { env } from "process";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
     context.log('HTTP trigger function processed a request.');
 
     context.res = {
-        body: { someField: "some value" }
+        body: { SearchServiceName: process.env.SearchServiceName }
     };
 
 };
