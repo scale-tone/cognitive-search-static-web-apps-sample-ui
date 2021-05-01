e1386c1dd333a44ef76b62d2f7f110ef40986f98
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-05-02 00:30:19 +0300

    more tests

--- api/HttpTest/index.ts
+++ api/HttpTest/index.ts
@@ -2,14 +2,9 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
-    for (var h in req.headers) {
-        
-        context.log('>>>' + h);
-    }
-
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: {}
+        body: req.headers['x-ms-client-principal']
     };
 
 };
