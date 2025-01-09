8ca2b1f7b7eb90e9d5a5e166b03bd85ca580ee88
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2025-01-09 23:14:26 +0100

    minor fix

--- api/autocomplete/index.ts
+++ api/autocomplete/index.ts
@@ -6,15 +6,23 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const searchApiUrl = `https://${process.env.SearchServiceName}.search.windows.net/indexes/${process.env.SearchIndexName}/docs/autocomplete?api-version=2019-05-06&`;
     const url = req.url.replace(/^http(s)?:\/\/[^/]+\/api\/autocomplete(\?)?/i, searchApiUrl);
 
-    const searchApiResponse = await axios.get(url, {
-        headers: {
-            "api-key": process.env.SearchApiKey,
-    } });
+    try {
+
+        const response = await axios.get(url, {
+            headers: {
+                "api-key": process.env.SearchApiKey,
+        } });
+        
+        context.res = {
+            status: response.status,
+            body: response.data,
+        };
     
-    context.res = {
-        status: searchApiResponse.status,
-        body: searchApiResponse.data,
-    };
+    } catch (err) {
+        context.res = {
+            status: err.response?.status ?? 500
+        };
+    }
 };
 
 export default httpTrigger;
